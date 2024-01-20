3bac3409e0f255178ca26f62b9e962a6d09042d6
akrem2005 <lighttech96@gmail.com>
2024-01-20 09:33:32 +0000

    first commit

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -6,6 +6,6 @@ router.get("/", courseController.getCourses);
 router.post("/new", courseController.createCourse);
 router.put("/:id", courseController.updateCourse);
 router.delete("/:id", courseController.deleteCourse);
-router.get("/:id", courseController.getCourseById);
+router.get("/:type", courseController.getCourseByType);
 router.post("/uploadVideo", courseController.uploadVideo);
 module.exports = router;
