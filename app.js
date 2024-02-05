a25add6a1259a21775fbb48b394ccb28f0255f20
akrem2005 <lighttech96@gmail.com>
2024-02-05 13:23:17 +0000

    page fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const categoryRoutes = require("./routes/categoryRoutes");
 const courseRoutes = require("./routes/courseRoutes");
 const userRoutes = require("./routes/userRoutes");
