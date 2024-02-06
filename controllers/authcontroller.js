12f564301b916c9a7b14d0b22677ca1ba5f30cfc
akrem2005 <lighttech96@gmail.com>
2024-02-06 19:16:16 +0000

    auth verfy 2 -- fix

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 // Middleware to check if a valid token is present
 exports.verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.Authorization;
 
   if (!token) {
     return res.status(401).json({ error: "Unauthorized: No token provided" });
@@ -15,33 +15,4 @@ exports.verifyToken = (req, res, next) => {
     req.user = decoded;
     next();
   });
-};
-exports.isAdmin = async (req, res, next) => {
-  try {
-    const token = req.body.token;
-
-    if (!token) {
-      return res.status(401).json({ error: "Unauthorized: No token provided" });
-    }
-
-    jwt.verify(token, "ardax", async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ error: "Unauthorized: Invalid token" });
-      }
-
-      // Check if the user has an admin role (customize based on your user model)
-      const user = await User.findById(decoded.userId);
-      if (!user || !user.isAdmin) {
-        return res
-          .status(403)
-          .json({ error: "Forbidden: User is not an admin" });
-      }
-
-      // If the user is an admin, proceed to the next middleware or route handler
-      next();
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to check admin status" });
-  }
 };
