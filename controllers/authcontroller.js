b53e8d5cb87d05cc0a60e5e526a594339fc72cb8
akrem2005 <lighttech96@gmail.com>
2024-02-07 09:03:09 +0000

    fix verify

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -11,3 +11,29 @@ exports.verifyToken = (req, res, next) => {
   }
   next();
 };
+exports.verifyTokenAndAdmin = (req, res, next) => {
+  const token = req.headers["authorization"].split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized - No token provided" });
+  }
+
+  // Verify the token
+  jwt.verify(token, "ardax", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Unauthorized - Invalid token" });
+    }
+
+    // Check if user is an admin
+    if (decoded.isAdmin) {
+      req.user = decoded; // Attach decoded user information to the request object
+      next();
+    } else {
+      return res
+        .status(403)
+        .json({ message: "Forbidden - User is not an admin" });
+    }
+  });
+};
