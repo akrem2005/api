27c803accc46368709221b396997d8d270efbd93
akrem2005 <lighttech96@gmail.com>
2024-02-07 14:57:19 +0000

    forbidn pages

--- app.js
+++ app.js
@@ -49,6 +49,15 @@ app.use("/pay", paymentRoutes);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+app.use((req, res, next) => {
+  res.status(500).sendFile(path.join(__dirname, "404.html"));
+});
+app.use((req, res, next) => {
+  res.status(403).sendFile(path.join(__dirname, "404.html"));
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
