bd1479b378ed9d69f6baa71fff74825de0ee501e
akrem2005 <lighttech96@gmail.com>
2024-02-07 08:40:16 +0000

    fix dns

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 // Middleware to check if a valid token is present
 exports.verifyToken = (req, res, next) => {
   const token = req.headers["authorization"].split(" ")[1];
-  console.log(token);
 
   if (!token) {
     return res
