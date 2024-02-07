61a1040a741a651128a826105ef9e30c6eb2fde5
akrem2005 <lighttech96@gmail.com>
2024-02-07 18:45:46 +0000

    notifications fix

--- models/Notification.js
+++ models/Notification.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const notificationSchema = new mongoose.Schema({
   text: String,
   date: String,
+  image: String,
+  subtitle: String,
   expires: { type: Date, default: Date.now, expires: 86400 }, // 86400 seconds = 24 hours
 });
 
