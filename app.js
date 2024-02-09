aa3ff8837be41eddad24d54b787a257fc4885d97
akrem2005 <lighttech96@gmail.com>
2024-02-09 16:45:07 +0000

    icon update

--- app.js
+++ app.js
@@ -22,29 +22,6 @@ app.use(express.json());
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/download", (req, res) => {
-  const filePath = path.join(
-    __dirname,
-    "apk",
-    "com.companyname.aradax-Signed.apk"
-  ); // Path to your local file
-
-  // Check if the file exists
-  if (fs.existsSync(filePath)) {
-    // Set the Content-Disposition header to trigger a download
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=com.companyname.aradax-Signed.apk"
-    );
-
-    // Stream the file as the response
-    const fileStream = fs.createReadStream(filePath);
-    fileStream.pipe(res);
-  } else {
-    res.status(404).send("File not found");
-  }
-});
-
 app.use("/categories", categoryRoutes);
 app.use("/courses", courseRoutes);
 app.use("/users", userRoutes);
@@ -55,7 +32,9 @@ app.use("/notifications", notificationsRoutes);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
-
+app.get("/overview", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "overview.html"));
+});
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
