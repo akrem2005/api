54c0e87b3707d1a8b6fb0d17ce72e8fdab7ee8d4
akrem2005 <lighttech96@gmail.com>
2024-02-20 17:33:10 +0000

    upload fix

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -20,7 +20,7 @@ const upload = multer({ storage: storage });
 
 // Handle HTML file upload
 exports.uploadFile = (req, res) => {
-  const file = req.file;
+  const file = req.body.file;
   if (!file) {
     return res.status(400).send("No file uploaded.");
   }
