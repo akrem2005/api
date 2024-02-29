ab95dd3bba669e3d59ed10bb7500ef747ebaad09
akrem2005 <lighttech96@gmail.com>
2024-02-29 16:44:03 +0000

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,7 +55,7 @@ exports.createUser = async (req, res) => {
       return res.status(400).json({ error: "User already exists" });
     }
     const saltRounds = 10;
-    const referal = otpGenerator.generate(6, {
+    const code = otpGenerator.generate(6, {
       digits: true,
       alphabets: true,
       upperCase: true,
@@ -72,8 +72,8 @@ exports.createUser = async (req, res) => {
         name: req.body.name,
         email: req.body.email,
         password: hashedPassword,
-        referal: "https://aradax.com.et/?referal=" + referal,
-        code: referal,
+        referal: "https://aradax.com.et/?code=" + code,
+        code: code,
         shares: "0",
       });
 
@@ -308,13 +308,13 @@ exports.getById = async (req, res) => {
 };
 exports.incrementSharesForReferringUser = async (req, res) => {
   try {
-    const { referal } = req.body;
+    const { code } = req.body;
 
-    if (!referal) {
+    if (!code) {
       return res.status(400).json({ error: "Referal code is required" });
     }
 
-    const referringUser = await User.findOneAndUpdate({ code: referal });
+    const referringUser = await User.findOneAndUpdate({ code: code });
 
     if (!referringUser) {
       return res.status(404).json({ error: "Referring user not found" });
